897de6b71150aa4e3d63958c1042077b654c18e7
Sofia Corzo <corzo@college.harvard.edu>
2018-11-27 10:08:00 -0500

    Adding rough code + app files by Nov. 27 meeting

---
+++ roughdraftcode.R
@@ -0,0 +1,17 @@
+#R Code just for rough draft of Shiny app
+
+# Loading relevant libraries
+library(tidyverse)
+library(ggplot2)
+library(readxl)
+
+# Loading datasets and renaming
+
+ucdp_prio_acd_181 <- read_excel("ucdp-prio-acd-181.xlsx")
+
+
+
+
+PCJ <- allconflicts
+MEPV <- MEPV2012ex
+
